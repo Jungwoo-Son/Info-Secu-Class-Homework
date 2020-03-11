3ee27e45d2f003c3213eecfc6b3c44be17842b25
Yeoul03 <jungwoo9356@gmail.com>
2020-03-11 22:46:57 +0900

    feat: add thread pool project
    I studied thread pool and made this used the rust language.

--- Rust_Study/rust_thread_pool/src/lib.rs
+++ Rust_Study/rust_thread_pool/src/lib.rs
@@ -1,8 +1,15 @@
 use std::thread;
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>
 }
+
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
@@ -10,18 +17,24 @@ impl ThreadPool {
 
         let mut workers = Vec::with_capacity(size);
 
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
         for id in 0..size {
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
         ThreadPool {
-            workers
+            workers,
+            sender,
         }
     }
 
     pub fn execute<F>(&self, _f: F)
         where F: FnOnce() + Send + 'static
     {
+        self.sender.send(Box::new(_f)).unwrap();
 
     }
 
@@ -33,8 +46,14 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            loop {
+                receiver.lock().unwrap().recv().unwrap()();
+            }
+
+        });
+
         Worker {
             id,
             thread,
--- Rust_Study/rust_thread_pool/src/main.rs
+++ Rust_Study/rust_thread_pool/src/main.rs
@@ -1,17 +1,23 @@
 extern crate rust_thread_pool;
 use rust_thread_pool::ThreadPool;
 
-use std::net::TcpListener;
+use std::time::Duration;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
-        let user = stream.unwrap();
-
         pool.execute(||{
-            println!("afsd");
+            println!("Hello");
         });
-    }
+        pool.execute(||{
+            println!("Hello");
+        });
+        pool.execute(||{
+            println!("Hello");
+        });
+        pool.execute(||{
+            println!("Hello");
+        });
+
+    std::thread::sleep(Duration::new(5, 2));
 }
