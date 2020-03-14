6ce4e5bacb39f2e4358bb90175738f2b72798011
A <jungwoo9356@gmail.com>
2020-03-15 01:15:06 +0900

    feat: Add client UI
    Now Client has beautiful UI

--- Rust_Project/Chatting_Program/chat_client/src/main.rs
+++ Rust_Project/Chatting_Program/chat_client/src/main.rs
@@ -4,7 +4,6 @@ use std::net::TcpStream;
 use std::thread;
 
 fn input_console() -> String{
-    print!("> ");
     std::io::stdout().flush().unwrap();
 
     let mut msg = String::new();
@@ -15,10 +14,8 @@ fn input_console() -> String{
 
 fn main() {
     if let Ok(mut stream) = TcpStream::connect("127.0.0.1:8085"){
-        println!("{}",stream.local_addr().unwrap().port());
         run_recv_chat(stream.try_clone().unwrap());
         loop {
-
             let msg = input_console();
             stream.write(msg.as_bytes()).unwrap();
         }
@@ -28,16 +25,71 @@ fn main() {
     }
 }
 
+use std::io::stdout;
+use crossterm::{execute, style, terminal, cursor};
+
+struct UiManager {
+    line: u16,
+}
+
+impl UiManager {
+    fn new() -> UiManager{
+
+        execute!(stdout(), cursor::MoveTo(0, terminal::size().unwrap().1 - 3)).unwrap();
+        UiManager::print_input_box();
+        execute!(stdout(), cursor::MoveTo(2, terminal::size().unwrap().1 - 2)).unwrap();
+        UiManager{
+            line: 0,
+        }
+    }
+    
+    fn print_input_box() {
+        let size_x = terminal::size().unwrap().0 - 1;
+        print!("┌ 입력 창");
+        for _ in 0..(size_x - 10) {
+            print!("─");
+        }
+        println!("┐");
+
+        print!("│");
+        for _ in 0..(size_x - 2) {
+            print!(" ");
+        } println!("│");
+
+        print!("└");
+        for _ in 0..(size_x - 2) {
+            print!("─");
+        } print!("┘");
+        std::io::stdout().flush().unwrap();
+    }
+
+    fn print_chat(&mut self, chat: &String) {
+        let term_y = terminal::size().unwrap().1;
+        execute!(stdout(), cursor::MoveTo(0, self.line)).unwrap();
+        println!("{}", chat);
+        if (term_y - 3) > self.line {
+            self.line += 1;
+            execute!(stdout(), cursor::MoveTo(0, term_y - 3)).unwrap();
+        }
+        UiManager::print_input_box();
+        execute!(stdout(), cursor::MoveTo(2, term_y - 2)).unwrap();
+    }
+}
+
+
+
+
 fn run_recv_chat(stream: TcpStream) {
     let mut stream = stream;
     thread::spawn(move || {
+        let mut ui_manager = UiManager::new();
         loop{
             let mut buffer = [0; 512];
-            stream.read(&mut buffer);
-            let data = String::from_utf8_lossy(&buffer);
-            println!("{}", data);
+            stream.read(&mut buffer).unwrap();
+            let data = String::from(String::from_utf8_lossy(&buffer));
+            //println!("{}", data);
+            ui_manager.print_chat(&data);
         }
     });
-
 }
 
