c00c33f95fa915d7098e8602a70841520ca89c12
Yeoul03 <jungwoo9356@gmail.com>
2020-03-15 20:01:23 +0900

    refactor: Split files that are client code
    Client code was an file. That's so hard to read. So I split the file and modularize to 'Ui', 'net', and 'console'.

--- Rust_Project/Chatting_Program/chat_client/src/main.rs
+++ Rust_Project/Chatting_Program/chat_client/src/main.rs
@@ -1,95 +1,38 @@
+extern crate chat_client;
+
 use std::io::prelude::*;
+
+use std::sync::mpsc::TryRecvError;
 use std::net::TcpStream;
 
-use std::thread;
-
-fn input_console() -> String{
-    std::io::stdout().flush().unwrap();
-
-    let mut msg = String::new();
-    std::io::stdin().read_line(&mut msg).unwrap();
-    let msg = String::from(msg.trim());
-    msg
-}
+use chat_client::net::run_recv_chat;
+use chat_client::ui::UiManager;
+use chat_client::console::run_input_console;
 
 fn main() {
     if let Ok(mut stream) = TcpStream::connect("127.0.0.1:8085"){
-        run_recv_chat(stream.try_clone().unwrap());
+        let mut ui_manager = UiManager::new();
+        let chat_receiver = run_recv_chat(stream.try_clone().unwrap());
+        let console_receiver = run_input_console();
+
         loop {
-            let msg = input_console();
-            stream.write(msg.as_bytes()).unwrap();
+            match console_receiver.try_recv() {
+                Ok(msg) => {stream.write(msg.as_bytes()).unwrap();},
+                Err(TryRecvError::Empty) => {},
+                _ => break,
+            };
+            
+            match chat_receiver.try_recv() {
+                Ok(chat_data) => ui_manager.print_chat(&chat_data),
+                Err(TryRecvError::Empty) => continue,
+                _ => break,
+            };
+
+
         }
+
     }
     else {
         println!("서버에 연결 할 수 없습니다");
     }
 }
-
-use std::io::stdout;
-use crossterm::{execute, style, terminal, cursor};
-
-struct UiManager {
-    line: u16,
-}
-
-impl UiManager {
-    fn new() -> UiManager{
-
-        execute!(stdout(), cursor::MoveTo(0, terminal::size().unwrap().1 - 3)).unwrap();
-        UiManager::print_input_box();
-        execute!(stdout(), cursor::MoveTo(2, terminal::size().unwrap().1 - 2)).unwrap();
-        UiManager{
-            line: 0,
-        }
-    }
-    
-    fn print_input_box() {
-        let size_x = terminal::size().unwrap().0 - 1;
-        print!("┌ 입력 창");
-        for _ in 0..(size_x - 10) {
-            print!("─");
-        }
-        println!("┐");
-
-        print!("│");
-        for _ in 0..(size_x - 2) {
-            print!(" ");
-        } println!("│");
-
-        print!("└");
-        for _ in 0..(size_x - 2) {
-            print!("─");
-        } print!("┘");
-        std::io::stdout().flush().unwrap();
-    }
-
-    fn print_chat(&mut self, chat: &String) {
-        let term_y = terminal::size().unwrap().1;
-        execute!(stdout(), cursor::MoveTo(0, self.line)).unwrap();
-        println!("{}", chat);
-        if (term_y - 3) > self.line {
-            self.line += 1;
-            execute!(stdout(), cursor::MoveTo(0, term_y - 3)).unwrap();
-        }
-        UiManager::print_input_box();
-        execute!(stdout(), cursor::MoveTo(2, term_y - 2)).unwrap();
-    }
-}
-
-
-
-
-fn run_recv_chat(stream: TcpStream) {
-    let mut stream = stream;
-    thread::spawn(move || {
-        let mut ui_manager = UiManager::new();
-        loop{
-            let mut buffer = [0; 512];
-            stream.read(&mut buffer).unwrap();
-            let data = String::from(String::from_utf8_lossy(&buffer));
-            //println!("{}", data);
-            ui_manager.print_chat(&data);
-        }
-    });
-}
-
