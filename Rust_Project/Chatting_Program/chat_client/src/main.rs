d64dab15b85a88fb69baf3b0bef3cbe06e3f594e
A <jungwoo9356@gmail.com>
2020-03-17 22:32:48 +0900

    docs: Add new file '템플릿 메타 프로그래밍.md'
    This file summarizes template meta programing in C++

--- Rust_Project/Chatting_Program/chat_client/src/main.rs
+++ Rust_Project/Chatting_Program/chat_client/src/main.rs
@@ -28,52 +28,11 @@ fn main() {
                 _ => break,
             };
 
-<<<<<<< HEAD
-impl UiManager {
-    fn new() -> UiManager{
-        UiManager::print_input_box();
-        UiManager{
-            line: 0,
-        }
-    }
-    
-    fn print_input_box() {
-        execute!(stdout(), cursor::MoveTo(0, terminal::size().unwrap().1 - 3)).unwrap();
-
-        let size_x = terminal::size().unwrap().0 - 1;
-        print!("┌ 입력 창");
-        for _ in 0..(size_x - 10) {
-            print!("─");
-        }
-        println!("┐");
-
-        print!("│");
-        for _ in 0..(size_x - 2) {
-            print!(" ");
-        } println!("│");
-
-        print!("└");
-        for _ in 0..(size_x - 2) {
-            print!("─");
-        } print!("┘");
-        std::io::stdout().flush().unwrap();
-        execute!(stdout(), cursor::MoveTo(2, terminal::size().unwrap().1 - 2)).unwrap();
-    }
-
-    fn print_chat(&mut self, chat: &String) {
-        let term_y = terminal::size().unwrap().1;
-        execute!(stdout(), cursor::MoveTo(0, self.line)).unwrap();
-        println!("{}", chat);
-        if (term_y - 3) > self.line {
-            self.line += 1;        }
-        UiManager::print_input_box();
-=======
 
         }
 
     }
     else {
         println!("서버에 연결 할 수 없습니다");
->>>>>>> c00c33f95fa915d7098e8602a70841520ca89c12
     }
 }
